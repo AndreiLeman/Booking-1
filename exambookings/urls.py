ff7d44798390b6007205f4b88edb4c4e37d4ea1d
Carson Cheng <mrccheng0@gmail.com>
2012-08-18 15:40:14 -0600

    converted CreateBooking into function based view and wired it up

--- exambookings/urls.py
+++ exambookings/urls.py
@@ -3,11 +3,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.generic import DetailView, ListView
-#from exambookings.models import Poll
-from exambookings.views import ShowBookings, CreateBooking
-
-
 urlpatterns = patterns('',
                        url(r'^home/$',
                            'exambookings.views.home_page',
@@ -22,7 +17,8 @@ urlpatterns = patterns('',
                            'exambookings.views.show_bookings',
                            name='showBookings'),
                        url(r'^create_booking/$',
-                           CreateBooking.as_view()),
+                           'exambookings.views.create_booking',
+                           name='createBooking'),
                        url(r'^static_page/(?P<file_name>.*\.html)$',
                            'exambookings.views.static_page'), # test out way to serve static page as though it were dynamic
 ) + static(settings.STATIC_URL, document_root='exambookings/static/')
--- exambookings/views.py
+++ exambookings/views.py
@@ -1,47 +1,35 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
-
-from django.views.generic import DetailView, ListView
-from django.views.generic.edit import CreateView
+from django.core.context_processors import csrf
 
 from exambookings.models import Booking
 from exambookings.forms import CreateBookingForm
+from userena.views import signin
 
 from django.core.urlresolvers import reverse
-from exambookings.viewsHelpers import reverse_lazy, StaffOnlyViewMixin, staff_only_view
+from exambookings.viewsHelpers import reverse_lazy, staff_only_view
 
 
-class CreateBooking(StaffOnlyViewMixin, CreateView):
-#    model = Booking
-#    template_name_suffix = "_create_form" # looks for template "booking_create_form.html"
-#    context_object_name = "create_booking"
-    template_name = 'exambookings/make_a_booking.html'
-    form_class = CreateBookingForm
-    success_url = reverse_lazy('showBookings')
+@staff_only_view
+def create_booking(request):
+    ctx = {}
+    ctx.update(csrf(request))
+    
+    form = CreateBookingForm()
+    if request.method == 'POST':
+        form = CreateBookingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.courseTeacher = request.user
+            form.save()
+            return HttpResponseRedirect(reverse('showBookings'))
+    
+    ctx['form'] = form
+    return render_to_response('exambookings/make_a_booking.html', ctx)
 
-    def get_initial(self):
-        """ this will set the initial value in the form, but
-        if form excludes this field, then when form is processed
-        courseTeacher field would not be set and can't be saved
-        into the database.  See form_valid instead.
-        """
-        return {'courseTeacher': self.request.user,}
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.        
-        form.instance.courseTeacher = self.request.user
-        return super(CreateBooking, self).form_valid(form)
-
-
-    # def render_to_response(self, context, **response_kwargs):
-    #     #return django.shortcuts.render_to_response('exambookings/make_a_booking.html', {})
-    #     context['forma'] = CreateBookingForm()
-    #     return super(CreateBooking, self).render_to_response(context, **response_kwargs)
 
 @staff_only_view
 def show_bookings(request):
@@ -66,13 +54,14 @@ def show_bookings(request):
     return render_to_response('exambookings/bookings_list.html',
                               {'bookings_list': bookings_list,})
 
-from userena.views import signin
+
 def home_page(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('showBookings'))
     return signin(request,
                   template_name='exambookings/home.html',
                   extra_context={'redirect_to':reverse('showBookings'),})
+
 
 def team_bio(request):
     return render_to_response('exambookings/static_pages/team_bio.html', {})
