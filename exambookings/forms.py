21e41a50cc86cb781a32c4763d04fa1743615751
Carson Cheng <mrccheng0@gmail.com>
2012-08-18 22:54:25 -0600

    make ExamBookingSignupForm handle first/last names and permission
    form now asks user for first/last names on signup
    first name is requested as initials only
    permission by default added is 'teacher_view'
    
    exam room staff will need manual adjustment of permission afterwards

--- exambookings/forms.py
+++ exambookings/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from exambookings.models import Booking
+from django.contrib.auth.models import Permission
 import userena.forms, re
 
 class CreateBookingForm(forms.ModelForm):
@@ -18,10 +19,27 @@ def email_is_allowed_signup(email):
     if (None != re.match(RE_PATTERN_example_com, email)) or (email in SIGNUP_EMAIL_WHITELIST):
         return True
     return False
-        
+
+
 class ExamBookingSignupForm(userena.forms.SignupFormOnlyEmail):
+    firstname = forms.CharField(max_length=30, label="First Name (Initial)")
+    lastname = forms.CharField(max_length=30, label="Last Name")
+
+    def __init__(self, *args, **kwargs):
+        super(ExamBookingSignupForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = ['firstname', 'lastname', 'email', 'password1', 'password2']
+    
     def clean_email(self):
         """ Validate that the e-mail address is allowed. """
         if not email_is_allowed_signup(self.cleaned_data['email']):
             raise forms.ValidationError('This email is not allowed. Please supply an authorized email.')
         return super(ExamBookingSignupForm, self).clean_email()
+
+    def save(self):
+        user = super(ExamBookingSignupForm, self).save()
+        user.user_permissions.add(Permission.objects.get(codename='teacher_view'))
+        user.first_name = self.cleaned_data['firstname']
+        user.last_name = self.cleaned_data['lastname']
+        user.save()
+        return user
+
