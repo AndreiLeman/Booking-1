d0f59032c99961f45e01670afdfded1a16e03d3b
Carson Cheng <mrccheng0@gmail.com>
2012-08-25 00:48:33 -0600

    wrote setup and application to run on OpenShift

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='ExamBookings',
+      version='0.0.3',
+      description='OpenShift ExamBookings',
+      author='cCheng',
+      author_email='ccheng@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django==1.3.1', 'django-userena==1.1.2', 'django-widget-tweaks==1.1.2'],
      )
