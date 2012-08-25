8f1f065811947a837c43e1d7b6adaf3ef8fec2ec
Carson Cheng <mrccheng0@gmail.com>
2012-08-25 01:58:34 -0600

    included PIL==1.1.7 as an install_requires

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='ExamBookings',
       author='cCheng',
       author_email='ccheng@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django==1.3.1', 'django-userena==1.1.2', 'django-widget-tweaks==1.1.2'],
+      install_requires=['Django==1.3.1', 'django-userena==1.1.2', 'django-widget-tweaks==1.1.2', 'PIL==1.1.7'],
      )
