ba0e3195d4222d2d5335c86fa6f2b6960eb80d7e
Carson Cheng <mrccheng0@gmail.com>
2012-08-25 00:47:43 -0600

    create secure_settings_safe_template.py, a blank secure_settings.py
    intended to be copied to BookingProj folder as secure_settings.py if
    one isn't there already

---
+++ data/secure_settings_safe_template.py
@@ -0,0 +1,9 @@
+# DO NOT COMMIT TO GIT or GITHUB REPOSITORIES
+# SECURE and LOCAL INFO
+#############################################
+
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
