c3267e4bc2d7a6ab0ad4f828593345d81a86cfc6
C Cheng <mrccheng0@gmail.com>
2012-09-02 20:41:50 -0600

    commented out useless import statements

--- wsgi/BookingProj/exambookings/views.py
+++ wsgi/BookingProj/exambookings/views.py
@@ -1,9 +1,9 @@
-from django.contrib import messages
+#from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
+#from django.template import RequestContext
 
 from django.contrib.auth.models import User
 from exambookings.models import Booking
